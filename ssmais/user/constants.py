de84cf3ce2f71b1881d079609bb188543d68382a
Igor Gabriel <igorgabriel515@gmail.com>
2017-11-25 23:02:54 -0200

    #15 - Creating User model.
    Signed-off-by: Hugo Alves <hugo.asb96@gmail.com>
    Signed-off-by: Igor Gabriel <igorgabriel515@gmail.com>

---
+++ ssmais/user/constants.py
@@ -0,0 +1,12 @@
+# EMAIL FIELDS.
+EMAIL_FIELD_LENGTH = 60
+
+# NAME FIELDS.
+NAME_FIELD_LENGTH = 80
+
+# EMAIL VALIDATION MESSAGES.
+EMAIL_FORMAT = 'Enter a valid email address'
+
+# NAME VALIDATION MESSAGES
+NAME_SIZE = 'Your name exceeds 60 characteres'
+NAME_CHARACTERS = 'Your name can\'t have special characters'
