4fc17ff45044300965d23314613f51b8ada5f598
Igor Gabriel <igorgabriel515@gmail.com>
2017-11-26 19:40:37 -0200

    #15 - Fixing user creation.

--- ssmais/search_scheduling/models/models_user.py
+++ ssmais/search_scheduling/models/models_user.py
@@ -1,6 +1,35 @@
 # Django.
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, name, **kwargs):
+        if not email:
+            raise ValueError('The given email must be set')
+
+        user = self.model(email=self.normalize_email(email),
+                          name=name,
+                          is_active=False,
+                          **kwargs)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password, name, **kwargs):
+        user = self.model(email=self.normalize_email(email),
+                          name=name,
+                          is_staff=True,
+                          is_active=True,
+                          is_superuser=True,
+                          **kwargs)
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -12,6 +41,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
+
+    objects = UserManager()
 
     def get_short_name(self):
         return self.name
