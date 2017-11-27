67f06caf78fade30d67380314e9c121ab9af7e3b
Thiago Nogueira <thiagonf10@gmail.com>
2017-11-27 00:05:06 -0200

    #14 - Fixing models provider.

--- ssmais/search_scheduling/models/models_provider.py
+++ ssmais/search_scheduling/models/models_provider.py
@@ -12,14 +12,7 @@ class Provider(models.Model):
 
 
 class OperatingHours(models.Model):
-    days_of_week = {'sunday', 'Sunday',
-                    'monday', 'Monday',
-                    'tuesday', 'Tuesday',
-                    'wednesday', 'Wednesday',
-                    'thirsday', 'Thirsday',
-                    'friday', 'Friday',
-                    'saturday', 'Saturday'}
     provider = models.ForeignKey('Provider', on_delete=models.CASCADE)
-    day = models.CharField(choice=days_of_week)
+    day = models.CharField(max_length=20)
     open_hour = models.TimeField()
     close_hour = models.TimeField()
